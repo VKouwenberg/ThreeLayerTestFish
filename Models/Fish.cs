8038ec9a7774f7d6f7a3c68fc780808e530c4ecf
McFinnigan <v.kouwenberg@student.fontys.nl>
2023-11-10 13:29:05 +0100

    Add project files.

---
+++ Models/Fish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CoreLogicFish.Models;
+
+public class Fish
+{
+    public string Name { get; private set; }
+    public int Caught { get; private set; }
+    public Fish(string name, int caught)
+    {
+        Name = name;
+        Caught = caught;
+    }
+}
---
+++ Services/FishService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreLogicFish.Models;
+using DataAccessFish.Repositories;
+
+namespace CoreLogicFish.Services;
+
+public class FishService
+{
+    private FishRepo repository = new();
+
+    public void FishCommands()
+    {
+        string command = "";
+        Console.WriteLine("Enter ADDFISH to add a fish");
+        Console.WriteLine("Enter LISTFISH to see all fish");
+        try
+        {
+            command = Convert.ToString(Console.ReadLine());
+        }
+        catch
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Error. Please enter a fish!");
+            Console.ResetColor();
+        }
+        switch (command)
+        {
+            case ("ADDFISH"):
+                repository.CreateNewFish();
+                break;
+            case ("LISTFISH"):
+                ListAllFish();
+                break;
+        }
+    }
+
+    public void ListAllFish()
+    {
+
+    }
+    /*public List<Fish> GetAllFish() 
+    {
+    }*/
+}
